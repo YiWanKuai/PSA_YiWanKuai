bd6e05c78054b1d47a7557b69ad76a85836f485f
khooroko <khoowp25@hotmail.com>
2017-09-20 16:27:13 +0800

    Ships now spawn their timer when they touch dock

--- Assets/Scripts/Stages/ShipController.cs
+++ Assets/Scripts/Stages/ShipController.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour {
-
-    private Rigidbody2D rb;
+    
     private GameManager gameManager;
-    private Animator anim;
 
+    private Animator anim;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+    private bool isDocked;
+
+    public GameObject timer;
     public float moveSpeed;
     public bool toTheLeft;
+    public float dockTime = 10f;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
         anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (gameManager.Unpaused()) {
+        if (gameManager.Unpaused() && !isDocked) {
             anim.enabled = true; 
             if (toTheLeft) {
                 rb.velocity = -moveSpeed * Axis.axis;
@@ -34,7 +40,23 @@ public class ShipController : MonoBehaviour {
 	}
 
     void OnTriggerEnter2D (Collider2D other) {
-        anim.SetTrigger("TurnLeft");
+        isDocked = true;
+        if(!toTheLeft) {
+            StartCoroutine(StartUnloading());
+        }
+
+        coll.enabled = false;
+    }
+
+    IEnumerator StartUnloading() {
+        GameObject newTimer = (GameObject) Instantiate(timer, transform, false);
+        ShipTimer timerScript = newTimer.GetComponent<ShipTimer>();
+        timerScript.time = dockTime;
+        yield return new WaitForSeconds(dockTime + 0.2f);
         toTheLeft = true;
+        anim.SetTrigger("TurnLeft");
+        isDocked = false;
+        yield return new WaitForSeconds(2f);
+        coll.enabled = true;
     }
 }
