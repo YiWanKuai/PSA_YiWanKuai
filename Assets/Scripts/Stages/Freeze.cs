b342b29db3597735f5f7595afd5dff87f74c35f3
khooroko <khoowp25@hotmail.com>
2017-10-06 17:08:50 +0800

    Add cooldown to freeze

--- Assets/Scripts/Stages/Freeze.cs
+++ Assets/Scripts/Stages/Freeze.cs
@@ -1,25 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Freeze : MonoBehaviour {
 
     private GameManager gm;
+    private Text text;
+    private Image image;
 
     // Use this for initialization
     void Start () {
-
+        text = GetComponentInChildren<Text>();
         gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        image = GetComponent<Image>();
     }
 
     public void FreezeGame() {
+        if(gm.Unpaused() && !gm.isFrozen)
         StartCoroutine(StartFreezeGame());
     }
 	
 	// Update is called once per frame
 	IEnumerator StartFreezeGame () {
         gm.isFrozen = true;
-        yield return new WaitForSeconds(PlayerPrefs.GetInt("freezeLevel") + 5f);
+        int waitTime = PlayerPrefs.GetInt("freezeLevel") + 5;
+        image.color = Color.gray;
+        while(waitTime > 0) {
+            text.text = waitTime.ToString();
+            yield return new WaitForSeconds(1f);
+            waitTime--;
+        }
+        image.color = Color.white;
         gm.isFrozen = false;
 	}
 }
